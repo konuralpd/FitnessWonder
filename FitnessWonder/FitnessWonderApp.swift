b1e3caefd7f4d2f7ff584eb670003e198837a7aa
konuralpd <98783085+konuralpd@users.noreply.github.com>
2022-07-20 23:08:04 +0300

    Initial Commit

---
+++ FitnessWonder/FitnessWonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessWonderApp.swift
+//  FitnessWonder
+//
+//  Created by Mac on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FitnessWonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
